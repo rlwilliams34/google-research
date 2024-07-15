b6f77254697ee075006dd23590635976e2fafd76
rlwilliams34 <rlwilliams34@g.ucla.edu>
2024-07-15 17:34:41 -0400

    No node feats in graphs

--- bigg/bigg/extension/main_featured.py
+++ bigg/bigg/extension/main_featured.py
@@ -85,10 +85,19 @@ if __name__ == '__main__':
     max_num_nodes = max([len(gg.nodes) for gg in train_graphs])
     cmd_args.max_num_nodes = max_num_nodes
     print('# graphs', len(train_graphs), 'max # nodes', max_num_nodes)
-
-    list_node_feats = [torch.from_numpy(get_node_feats(g)).to(cmd_args.device) for g in train_graphs]
-    list_edge_feats = [torch.from_numpy(get_edge_feats(g)).to(cmd_args.device) for g in train_graphs]
-
+    
+    if cmd_args.has_node_feats:
+        list_node_feats = [torch.from_numpy(get_node_feats(g)).to(cmd_args.device) for g in train_graphs]
+    
+    else:
+        list_node_feats = None
+    
+    if cmd_args.has_edge_feats:
+        list_edge_feats = [torch.from_numpy(get_edge_feats(g)).to(cmd_args.device) for g in train_graphs]
+    
+    else:
+        list_edge_feats = None
+    
     model = BiggWithEdgeLen(cmd_args).to(cmd_args.device)
     optimizer = optim.Adam(model.parameters(), lr=cmd_args.learning_rate, weight_decay=1e-4)
     
