a60838203e69fcc069dbd3fbeafef698aa59f799
rlwilliams34 <rlwilliams34@g.ucla.edu>
2024-08-23 12:30:20 -0700

    test

--- bigg/bigg/model/tree_model.py
+++ bigg/bigg/model/tree_model.py
@@ -77,7 +77,7 @@ def selective_update_hc(h, c, zero_one, feats, cell_edge=None):
         c = torch.where(zero_one, local_edge_feats, c)
     
     else:
-        edge_state = (h[np.expand_dims(zero_one, -1)], c[np.expand_dims(zero_one, -1)])
+        edge_state = (h, c)#(h[np.expand_dims(zero_one, -1)], c[np.expand_dims(zero_one, -1)])
         print(h.shape)
         print(h[zero_one].shape)
         print(zero_one.shape)
