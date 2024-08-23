7ff140585e775e567302a9738c6425437921e7e4
rlwilliams34 <rlwilliams34@g.ucla.edu>
2024-08-23 14:24:03 -0700

    back to MLP

--- bigg/bigg/extension/customized_models.py
+++ bigg/bigg/extension/customized_models.py
@@ -31,10 +31,9 @@ class BiggWithEdgeLen(RecurTreeGen):
     def __init__(self, args):
         super().__init__(args)
         self.edgelen_encoding = MLP(1, [args.embed_dim // 4, args.embed_dim]) 
-        self.edge_state_update = nn.LSTMCell(args.embed_dim, args.embed_dim)
         self.nodelen_encoding = MLP(1, [2 * args.embed_dim, args.embed_dim])
         self.nodelen_pred = MLP(args.embed_dim, [2 * args.embed_dim, 1])
-        #self.edgelen_pred = MLP(args.embed_dim, [2 * args.embed_dim, 1])
+        
         self.edgelen_mean = MLP(args.embed_dim, [2 * args.embed_dim, 1])
         self.edgelen_lvar = MLP(args.embed_dim, [2 * args.embed_dim, 1])
         self.node_state_update = nn.LSTMCell(args.embed_dim, args.embed_dim)
@@ -86,6 +85,9 @@ class BiggWithEdgeLen(RecurTreeGen):
       
       elif self.mode == "exp":
         weights = torch.exp(-1/weights)
+      
+      elif self.mode == "exp-log":
+        weights = torch.exp(-1 / torch.log(1 + weights))
         
       return weights
   
