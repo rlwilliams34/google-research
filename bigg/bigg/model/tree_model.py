73dd3f41d66b71ef74bf6df5b7e602bc02508763
rlwilliams34 <rlwilliams34@g.ucla.edu>
2024-08-26 16:07:38 -0700

    working on multi layer

--- bigg/bigg/model/tree_model.py
+++ bigg/bigg/model/tree_model.py
@@ -642,6 +642,9 @@ class RecurTreeGen(nn.Module):
         hc_bot, fn_hc_bot, h_buf_list, c_buf_list = self.forward_row_trees(graph_ids, node_feats, edge_feats,
                                                                            list_node_starts, num_nodes, list_col_ranges)
         row_states, next_states = self.row_tree.forward_train(*hc_bot, h_buf_list[0], c_buf_list[0], *prev_rowsum_states)
+        print(row_states)
+        print(next_states)
+        
         if self.has_node_feats:
             row_states, ll_node_feats, _ = self.predict_node_feats(row_states, node_feats)
             ll = ll + ll_node_feats
