b788e208059699aebac1761e2fb555cebdd49dbe
rlwilliams34 <rlwilliams34@g.ucla.edu>
2024-08-26 19:44:16 -0700

    fix typo in prediction

--- bigg/bigg/extension/main_featured.py
+++ bigg/bigg/extension/main_featured.py
@@ -319,7 +319,7 @@ if __name__ == '__main__':
             checkpoint = {'epoch': epoch, 'model': model.state_dict(), 'optimizer': optimizer.state_dict()}
             torch.save(checkpoint, os.path.join(cmd_args.save_dir, 'epoch-%d.ckpt' % (epoch + 1)))
         
-        if cur % cmd_args.epoch_save == 0 and cur >= 20:
+        if cur % cmd_args.epoch_save == 0 and cur >= 20 and cmd_args.has_edge_feats:
             print('validating')
             
             gen_graphs = []
