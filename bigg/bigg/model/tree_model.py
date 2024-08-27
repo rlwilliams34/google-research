cdf71bf0e3dcb3e9db976d553a3f31692484aa2b
rlwilliams34 <rlwilliams34@g.ucla.edu>
2024-08-27 07:33:45 -0700

    remove print

--- bigg/bigg/model/tree_model.py
+++ bigg/bigg/model/tree_model.py
@@ -71,7 +71,6 @@ def batch_tree_lstm2(h_bot, c_bot, h_buf, c_buf, fn_all_ids, cell):
 def selective_update_hc(h, c, zero_one, feats):
     nz_idx = torch.tensor(np.nonzero(zero_one)[0]).to(h.device)
     local_edge_feats = scatter(feats, nz_idx, dim=0, dim_size=h.shape[1])
-    print(zero_one)
     zero_one = torch.tensor(zero_one, dtype=torch.bool).to(h.device).unsqueeze(1)
     h = torch.where(zero_one, local_edge_feats, h)
     c = torch.where(zero_one, local_edge_feats, c)
