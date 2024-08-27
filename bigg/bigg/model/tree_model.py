8e1fc6114a92ec3d879137ae44e1dade608738f2
rlwilliams34 <rlwilliams34@g.ucla.edu>
2024-08-27 07:50:50 -0700

    fix

--- bigg/bigg/model/tree_model.py
+++ bigg/bigg/model/tree_model.py
@@ -463,9 +463,9 @@ class RecurTreeGen(nn.Module):
                     edge_ll, cur_feats = self.predict_edge_feats(state, cur_feats)
                     ll = ll + edge_ll
                     edge_embed = self.embed_edge_feats(cur_feats)
-                    print(edge_embed.shape)
+                    
                     edge_embed = edge_embed.unsqueeze(1)
-                    #eded_embed = edge_embed.repeat(self.num_layers, 1)
+                    eded_embed = edge_embed.repeat(self.num_layers, 1)
                     
                     return ll, (edge_embed, edge_embed), 1, cur_feats
                 else:
