50f9c87209298d2da879f97523839a41343dfd94
rlwilliams34 <rlwilliams34@g.ucla.edu>
2024-08-26 17:16:17 -0700

    testing

--- bigg/bigg/torch_ops/tensor_ops.py
+++ bigg/bigg/torch_ops/tensor_ops.py
@@ -30,13 +30,8 @@ class MultiIndexSelectFunc(Function):
     @staticmethod
     def forward(ctx, idx_froms, idx_tos, *mats):
         assert len(idx_tos) == len(idx_froms) == len(mats)
-        print(ctx)
-        print(idx_froms)
-        print(idx_tos)
-        print(mats)
-        print(len(idx_tos))
-        print(len(idx_froms))
-        print(len(mats))
+        print(mats[0])
+        print(mats[0].shape)
         print("Stop 1")
         cols = mats[0].shape[2]
         #assert all([len(x.shape) == 2 for x in mats])
