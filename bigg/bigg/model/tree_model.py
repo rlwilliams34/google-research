e3000f5ee261ead32f085dd8b07caa9a85a94362
rlwilliams34 <rlwilliams34@g.ucla.edu>
2024-08-26 17:47:01 -0700

    test

--- bigg/bigg/model/tree_model.py
+++ bigg/bigg/model/tree_model.py
@@ -210,9 +210,9 @@ class FenwickTree(nn.Module):
     def forward_train(self, h_bot, c_bot, h_buf0, c_buf0, prev_rowsum_h, prrev_rowsum_c):
         # embed row tree
         tree_agg_ids = TreeLib.PrepareRowEmbed()
-        print("Test")
-        print(list(range(tree_agg_ids)))
-        print("Done")
+        #print("Test")
+        #print(list(range(tree_agg_ids)))
+        #print("Done")
         row_embeds = [(self.init_h0, self.init_c0)]
         if self.has_edge_feats or self.has_node_feats:
             feat_dict = c_bot
