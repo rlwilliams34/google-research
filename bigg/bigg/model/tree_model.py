32edc24a11027dd092d0c56e8421b0abbfb32410
rlwilliams34 <rlwilliams34@g.ucla.edu>
2024-08-26 19:03:22 -0700

    fix

--- bigg/bigg/model/tree_model.py
+++ bigg/bigg/model/tree_model.py
@@ -681,7 +681,7 @@ class RecurTreeGen(nn.Module):
             if is_nonleaf is None or np.sum(is_nonleaf) == 0:
                 break
             cur_states = (cur_states[0][:, is_nonleaf], cur_states[1][:, is_nonleaf])
-            left_logits = self.pred_has_left(cur_states[0], lv)
+            left_logits = self.pred_has_left(cur_states[0][-1], lv)
             has_left, num_left = TreeLib.GetChLabel(-1, lv)
             left_update = self.topdown_left_embed[has_left] + self.tree_pos_enc(num_left)
             left_ll, float_has_left = self.binary_ll(left_logits, has_left, need_label=True, reduction='sum')
