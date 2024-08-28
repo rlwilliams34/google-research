5bd175cb8a56b7d83dedebd7b2e43a0337d7cb72
rlwilliams34 <rlwilliams34@g.ucla.edu>
2024-08-28 09:57:17 -0700

    fix

--- bigg/bigg/model/tree_model.py
+++ bigg/bigg/model/tree_model.py
@@ -470,7 +470,7 @@ class RecurTreeGen(nn.Module):
                     edge_embed = self.embed_edge_feats(cur_feats)
                     
                     #edge_embed = edge_embed.repeat(self.num_layers, 1).unsqueeze(1)
-                    edge_embed = edge_embed.reshape(self.num_layers, self.embed_dim).unsqueeze(1)
+                    #edge_embed = edge_embed.reshape(self.num_layers, self.embed_dim).unsqueeze(1)
                     
                     ## LSTM: Dimensionality == (# Layers, Embed Size)
                     
