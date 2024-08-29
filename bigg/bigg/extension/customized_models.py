7b66736d38c9e2d6c537da9ffe3607ac7fb0d756
rlwilliams34 <rlwilliams34@g.ucla.edu>
2024-08-28 18:22:46 -0700

    fix

--- bigg/bigg/extension/customized_models.py
+++ bigg/bigg/extension/customized_models.py
@@ -160,7 +160,8 @@ class BiggWithEdgeLen(RecurTreeGen):
         edge_embed = self.edgelen_encoding(edge_feats_normalized)
         #state = self.edgeLSTM(edge_embed, (self.leaf_h0.repeat(1, edge_embed.shape[0], 1), self.leaf_c0.repeat(1, edge_embed.shape[0],1)))
         #return state
-        out = edge_embed.reshape(edge_feats.shape[0], self.num_layers, self.embed_dim).movedim(0, 1)
+        print(edge_feats.shape)
+        out = edge_embed.reshape(edge_feats.shape[1], self.num_layers, self.embed_dim).movedim(0, 1)
         out_h = torch.cat([out, self.leaf_h0], dim = 0)
         out_c = torch.cat([out, self.leaf_c0], dim = 0)
         return (out_h, out_c) #self.edgelen_encoding(edge_feats_normalized)
