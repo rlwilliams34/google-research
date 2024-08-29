9cc70deb75c753a2259aab97090d685970e3ebdd
rlwilliams34 <rlwilliams34@g.ucla.edu>
2024-08-29 09:20:51 -0700

    use MLP

--- bigg/bigg/extension/customized_models.py
+++ bigg/bigg/extension/customized_models.py
@@ -36,7 +36,7 @@ class BiggWithEdgeLen(RecurTreeGen):
             self.leaf_h0_wt = Parameter(torch.Tensor(args.rnn_layers, 1, args.embed_dim // 2))
             self.leaf_c0_wt = Parameter(torch.Tensor(args.rnn_layers, 1, args.embed_dim // 2))
             self.edgeLSTM = MultiLSTMCell(1, args.embed_dim, args.rnn_layers)
-            #self.edgelen_encoding = MLP(1, [2 * args.embed_dim, args.embed_dim // 16])
+            self.edgelen_encoding = MLP(1, [32, 16])
         else:
             self.edgelen_encoding = MLP(1, [2 * args.embed_dim, args.embed_dim])
         self.nodelen_encoding = MLP(1, [2 * args.embed_dim, args.embed_dim])
@@ -161,7 +161,7 @@ class BiggWithEdgeLen(RecurTreeGen):
         if self.epoch_num == 0:
             self.update_weight_stats(edge_feats)
         edge_feats_normalized = self.standardize_weights(edge_feats)
-        edge_embed = edge_feats_normalized #self.edgelen_encoding(edge_feats_normalized)
+        edge_embed = self.edgelen_encoding(edge_feats_normalized)
         state = self.edgeLSTM(edge_embed, (self.leaf_h0.repeat(1, edge_embed.shape[0], 1), self.leaf_c0.repeat(1, edge_embed.shape[0],1)))
         return state
         out = edge_embed.reshape(edge_feats.shape[0], self.num_layers, self.embed_dim // 2).movedim(0, 1)
