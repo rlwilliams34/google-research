b12a7cd9782c100f21cadd956467e1ee1e5e809d
rlwilliams34 <rlwilliams34@g.ucla.edu>
2024-08-30 09:57:55 -0700

    fix

--- bigg/bigg/extension/customized_models.py
+++ bigg/bigg/extension/customized_models.py
@@ -198,9 +198,9 @@ class BiggWithEdgeLen(RecurTreeGen):
         if self.method == "MLP-Leaf":
             edge_embed = self.edgelen_encoding(edge_feats_normalized)
             out = edge_embed.reshape(edge_feats.shape[0], self.num_layers, self.embed_dim // 2).movedim(0, 1)
-            out_h = torch.cat([out, self.leaf_h0_wt.repeat(1, edge_feats.shape[0], 1)], dim = -1)
-            out_c = torch.cat([out, self.leaf_c0_wt.repeat(1, edge_feats.shape[0], 1)], dim = -1)
-            print(out_h)
+            out_h = torch.cat([self.leaf_h0_wt.repeat(1, edge_feats.shape[0], 1), out], dim = -1)
+            out_c = torch.cat([self.leaf_c0_wt.repeat(1, edge_feats.shape[0], 1), out], dim = -1)
+            print(out)
             return (out_h, out_c) 
 
     def predict_node_feats(self, state, node_feats=None):
