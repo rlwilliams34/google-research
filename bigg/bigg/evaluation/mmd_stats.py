27622d286c6b307b42cc7bba8119d2163e70412b
rlwilliams34 <rlwilliams34@g.ucla.edu>
2024-09-06 14:55:41 -0700

    fixing orca

--- bigg/bigg/evaluation/mmd_stats.py
+++ bigg/bigg/evaluation/mmd_stats.py
@@ -125,14 +125,14 @@ def edge_list_reindexed(G):
     return edges
 
 def orca(graph):
-    path = '/u/home/r/rlwillia/google-research/bigg/bigg/evaluation/orca/tmp.txt'
+    path = '../../evaluation/orca/tmp.txt'
     f = open(path, 'w')
     f.write(str(graph.number_of_nodes()) + ' ' + str(graph.number_of_edges()) + '\n')
     for (u, v) in edge_list_reindexed(graph):
         f.write(str(u) + ' ' + str(v) + '\n')
     f.close()
     
-    output = sp.check_output(['/u/home/r/rlwillia/google-research/bigg/bigg/evaluation/orca/orca', 'node', '4', path, 'std'])
+    output = sp.check_output(['../../evaluation/orca/orca', 'node', '4', path, 'std'])
     output = output.decode('utf8').strip()
     
     idx = output.find(COUNT_START_STR) + len(COUNT_START_STR)
