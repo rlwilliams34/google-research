7d6144951c4d08e907ec0255373954263609e632
rlwilliams34 <rlwilliams34@g.ucla.edu>
2024-10-03 21:49:18 -0700

    test

--- bigg/bigg/experiments/train_utils.py
+++ bigg/bigg/experiments/train_utils.py
@@ -69,7 +69,7 @@ def sqrtn_forward_backward(model,
                 print(node_st)
                 print(cur_num)
                 cur_edge_idx = (edge_idx>=node_st)&(edge_idx<node_st+cur_num)
-                cur_edge_feats = edge_feats[cur_edge_idx]
+                cur_edge_feats = edge_feats
             print(count_)
             count_+=1
             print("Edge Feats Shape")
@@ -101,7 +101,7 @@ def sqrtn_forward_backward(model,
             print(node_st)
             print(cur_num)
             cur_edge_idx = (edge_idx>=node_st)&(edge_idx<node_st+cur_num)
-            cur_edge_feats = edge_feats[cur_edge_idx]
+            cur_edge_feats = edge_feats
         
         ll, ll_wt, cur_states = model.forward_train(graph_ids,
                                              list_node_starts=[node_st],
