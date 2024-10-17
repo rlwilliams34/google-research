74521c34b089aef34f4b5525f44922b79afecfe3
rlwilliams34 <rlwilliams34@g.ucla.edu>
2024-10-17 08:12:52 -0700

    test

--- bigg/bigg/extension/main_featured.py
+++ bigg/bigg/extension/main_featured.py
@@ -276,6 +276,7 @@ if __name__ == '__main__':
             for _ in tqdm(range(cmd_args.num_test_gen)):
                 num_nodes = np.argmax(np.random.multinomial(1, num_node_dist)) 
                 _, pred_edges, _, pred_node_feats, pred_edge_feats = model(node_end = num_nodes, display=cmd_args.display)
+                print(pred_edge_feats)
                 
                 if cmd_args.model == "BiGG_GCN":
                     fix_edges = []
