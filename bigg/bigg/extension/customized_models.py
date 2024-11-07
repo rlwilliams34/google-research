661a8b92560e66d66aba879400b908c076723966
rlwilliams34 <rlwilliams34@g.ucla.edu>
2024-11-06 16:12:23 -0800

    test

--- bigg/bigg/extension/customized_models.py
+++ bigg/bigg/extension/customized_models.py
@@ -219,6 +219,7 @@ class BiggWithEdgeLen(RecurTreeGen):
         
         if self.method == "MLP-Repeat":
             if len(edge_feats.flatten()) > 1:
+                print(edge_feats_normalized)
                 edge_embed = self.edgelen_encoding(edge_feats_normalized + 0.1 * torch.randn_like(edge_feats_normalized))
             
             else:
