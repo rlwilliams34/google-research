eb8c8e8ce18462c8456de35f0c716d9358bf6d2a
rlwilliams34 <rlwilliams34@g.ucla.edu>
2024-11-08 13:16:49 -0800

    fix

--- bigg/bigg/extension/customized_models.py
+++ bigg/bigg/extension/customized_models.py
@@ -262,8 +262,8 @@ class BiggWithEdgeLen(RecurTreeGen):
                 states_h = []
                 states_c = []
                 for list_edge in edge_feats_normalized:
-                    print(edge_feats_normalized)
-                    edge_embed = self.edgelen_encoding(edge_feats_normalized)
+                    #print(edge_feats_normalized)
+                    edge_embed = self.edgelen_encoding(list_edge)
                     cur_state = (self.leaf_h0_wt, self.leaf_c0_wt)
                     
                     for edge in edge_embed:
