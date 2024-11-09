055886b3c0642250cf26dc6c59b5c934e0961947
rlwilliams34 <rlwilliams34@g.ucla.edu>
2024-11-08 18:15:06 -0800

    fix

--- bigg/bigg/model/tree_model.py
+++ bigg/bigg/model/tree_model.py
@@ -755,9 +755,9 @@ class RecurTreeGen(nn.Module):
             #noise = 0.1 * torch.randn_like(edge_feats).to(edge_feats.device)
             
             if self.method == "LSTM":
-                edge_feats = torch.cat(edge_feats, dim = 0)
+                #edge_feats = torch.cat(edge_feats, dim = 0)
                 edge_feats_embed = self.embed_edge_feats(edge_feats, noise)
-                print(edge_feats_embed[0].shape)
+                #print(edge_feats_embed[0].shape)
             
             else:
                 edge_feats_embed = self.embed_edge_feats(edge_feats, noise)
