8671f19d39fe143a2858cdc98235b14de169d521
rlwilliams34 <rlwilliams34@g.ucla.edu>
2024-11-09 19:48:55 -0800

    test

--- bigg/bigg/model/tree_model.py
+++ bigg/bigg/model/tree_model.py
@@ -110,7 +110,7 @@ def featured_batch_tree_lstm2(edge_feats, is_rch, h_bot, c_bot, h_buf, c_buf, fn
     #print(STOP)
     for i in range(2):
         leaf_check = is_leaf[i]
-        if sum(leaf_check) == 0 or wt_upate is None:
+        if sum(leaf_check) == 0 or wt_update is None:
             continue
         cur_summary = (summary_state[0][:, leaf_check], summary_state[1][:, leaf_check])
         cur_edge_feats = edge_feats[i][0]
