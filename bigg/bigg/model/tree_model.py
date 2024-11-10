b0c136178dc5e04a6e61d83e5f2b120b64456797
rlwilliams34 <rlwilliams34@g.ucla.edu>
2024-11-10 08:29:48 -0800

    fix

--- bigg/bigg/model/tree_model.py
+++ bigg/bigg/model/tree_model.py
@@ -128,13 +128,13 @@ def featured_batch_tree_lstm2(edge_feats, is_rch, h_bot, c_bot, h_buf, c_buf, fn
         #print("summary_state: ", summary_state[0])
         cur_summary = (local_hbot, local_cbot)
         cur_edge_feats = edge_feats[i]
-        print("==========================================")
-        print("CURRENT EDGE FEATS AND SUMMARY TO BE UPDATED")
-        print(cur_edge_feats)
-        print("Before: ", cur_summary)
+        #print("==========================================")
+        #print("CURRENT EDGE FEATS AND SUMMARY TO BE UPDATED")
+        #print(cur_edge_feats)
+        #print("Before: ", cur_summary)
         cur_summary = wt_update(cur_edge_feats, cur_summary)
-        print("After: ", cur_summary)
-        print("==========================================")
+        #print("After: ", cur_summary)
+        #print("==========================================")
         
         summary_state[0][:, local_idx] = cur_summary[0]
         summary_state[1][:, local_idx] = cur_summary[1]
@@ -640,25 +640,25 @@ class RecurTreeGen(nn.Module):
             if self.has_edge_feats:
                 edge_feats = torch.cat(pred_edge_feats, dim=0)
                 if has_left and tree_node.lch.is_leaf:
-                    print("==========================================")
-                    print("HAS LEFT")
+                    #print("==========================================")
+                    #print("HAS LEFT")
                     left_edge_embed = self.embed_edge_feats(left_edge_feats, prev_state=prev_wt_state)
-                    print(left_edge_feats)
-                    print(left_edge_embed)
-                    print("Before: ", summary_state)
+                    #print(left_edge_feats)
+                    #print(left_edge_embed)
+                    #print("Before: ", summary_state)
                     summary_state = self.update_wt(left_edge_embed, summary_state)
-                    print("After: ", summary_state)
-                    print("==========================================")
+                    #print("After: ", summary_state)
+                    #print("==========================================")
                 if has_right and tree_node.rch.is_leaf:
-                    print("==========================================")
-                    print("HAS RIGHT")
+                    #print("==========================================")
+                    #print("HAS RIGHT")
                     right_edge_embed = self.embed_edge_feats(right_edge_feats, prev_state=prev_wt_state)
-                    print(right_edge_feats)
-                    print(right_edge_embed)
-                    print("Before: ", summary_state)
+                    #print(right_edge_feats)
+                    #print(right_edge_embed)
+                    #print("Before: ", summary_state)
                     summary_state = self.update_wt(right_edge_embed, summary_state)
-                    print("AFter: ", summary_state)
-                    print("==========================================")
+                    #print("AFter: ", summary_state)
+                    #print("==========================================")
             return ll, ll_wt, summary_state, num_left + num_right, edge_feats, prev_wt_state
 
     def forward(self, node_end, edge_list=None, node_feats=None, edge_feats=None, node_start=0, list_states=[], lb_list=None, ub_list=None, col_range=None, num_nodes=None, display=False):
@@ -706,8 +706,8 @@ class RecurTreeGen(nn.Module):
                 target_edge_feats = None if edge_feats is None else edge_feats[len(edges) : len(edges) + len(col_sm)]
             else:
                 target_edge_feats = None
-            print(i)
-            print(controller_state)
+            #print(i)
+            #print(controller_state)
             ll, ll_wt, cur_state, _, target_edge_feats, prev_wt_state = self.gen_row(0, 0, controller_state, cur_row.root, col_sm, lb, ub, target_edge_feats, prev_wt_state)
 #             if i == 1 and target_edge_feats is not None:
 #                 edge_embed = self.embed_edge_feats(target_edge_feats, prev_state=prev_wt_state)
@@ -848,7 +848,7 @@ class RecurTreeGen(nn.Module):
                                                                            list_node_starts, num_nodes, list_col_ranges, noise, edge_feats_embed)
         
         row_states, next_states = self.row_tree.forward_train(*hc_bot, h_buf_list[0], c_buf_list[0], *prev_rowsum_states, wt_update=self.update_wt)
-        print(row_states)
+        #print(row_states)
         if self.has_node_feats:
             row_states, ll_node_feats, _ = self.predict_node_feats(row_states, node_feats)
             ll = ll + ll_node_feats
@@ -862,8 +862,8 @@ class RecurTreeGen(nn.Module):
             
             else:
                 edge_feats_embed = self.embed_edge_feats(edge_feats, noise)
-                print(edge_feats)
-                print(edge_feats_embed)
+                #print(edge_feats)
+                #print(edge_feats_embed)
         
         logit_has_edge = self.pred_has_ch(row_states[0][-1])
         has_ch, _ = TreeLib.GetChLabel(0, dtype=bool)
