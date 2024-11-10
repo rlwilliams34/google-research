73c956e8241c5197c7d6559d44c27df7bd0f9874
rlwilliams34 <rlwilliams34@g.ucla.edu>
2024-11-10 14:18:53 -0800

    fix

--- bigg/bigg/extension/main_featured.py
+++ bigg/bigg/extension/main_featured.py
@@ -442,7 +442,7 @@ if __name__ == '__main__':
             
             node_feats = (torch.cat([list_node_feats[i] for i in batch_indices], dim=0) if list_node_feats is not None else None)
             
-            if cmd_args.method == "LSTM" and cmd_args.has_edge_feats:
+            if cmd_args.method in ["LSTM", "MLP-Leaf"] and cmd_args.has_edge_feats:
                 #edge_feats_embed_h = (torch.cat([list_edge_feats_embed[0][i] for i in batch_indices], dim=1)) #[list_edge_feats[i] for i in batch_indices]
                 #edge_feats_embed_c = (torch.cat([list_edge_feats_embed[1][i] for i in batch_indices], dim=1)) #[list_edge_feats[i] for i in batch_indices]
                 #edge_feats_embed = (edge_feats_embed_h, edge_feats_embed_c)
