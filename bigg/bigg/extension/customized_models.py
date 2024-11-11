115e5d1d1b8978b5878b1948c1e06063849c8fe3
rlwilliams34 <rlwilliams34@g.ucla.edu>
2024-11-11 10:37:54 -0800

    check

--- bigg/bigg/extension/customized_models.py
+++ bigg/bigg/extension/customized_models.py
@@ -255,13 +255,12 @@ class BiggWithEdgeLen(RecurTreeGen):
                 for edge in edge_embed:
                     prev_states_h.append(cur_state[0])
                     cur_state = self.edgeLSTM(edge, cur_state)
-                    print(cur_state[0].shape)
                     states_h.append(cur_state[0])
                     states_c.append(cur_state[1])       
                 state_h = torch.cat(states_h, 0)
-                print(state_h.shape)
                 state_c = torch.cat(states_c, 0) 
                 prev_h = torch.cat(prev_states_h, dim = -1).view(state_h.shape[0], state_h.shape[1])
+                print(prev_h.shape)
                 state = (state_h, state_c) 
                 return state, prev_h
                 
