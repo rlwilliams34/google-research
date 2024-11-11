13fc3903e9d4815e34249c2f1da5447333bc4571
rlwilliams34 <rlwilliams34@g.ucla.edu>
2024-11-11 10:55:43 -0800

    fix

--- bigg/bigg/extension/main_featured.py
+++ bigg/bigg/extension/main_featured.py
@@ -418,16 +418,16 @@ if __name__ == '__main__':
                 debug_model(model, train_graphs[0], None, None)
         
         
-#         if epoch == 0 and cmd_args.has_edge_feats and cmd_args.model == "BiGG_E":
-#             for i in range(len(list_edge_feats)):
-#                 edge_feats = list_edge_feats[i]
-#                 model.update_weight_stats(edge_feats)
-#         
-#         if cmd_args.model == "BiGG_GCN":
-#             model.gcn_mod.epoch_num += 1
-#         
-#         else:
-#             model.epoch_num += 1
+        if epoch == 0 and cmd_args.has_edge_feats and cmd_args.model == "BiGG_E":
+            for i in range(len(list_edge_feats)):
+                edge_feats = list_edge_feats[i]
+                model.update_weight_stats(edge_feats)
+        
+        if cmd_args.model == "BiGG_GCN":
+            model.gcn_mod.epoch_num += 1
+        
+        else:
+            model.epoch_num += 1
         
         if cmd_args.schedule:
             if epoch >= epoch_lr_decrease and cmd_args.learning_rate != 1e-5:
