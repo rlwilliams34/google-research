6aedd7e1ddc132c40fcce30cab4819d8d8c5afda
rlwilliams34 <rlwilliams34@g.ucla.edu>
2024-11-12 08:37:52 -0800

    Test

--- bigg/bigg/extension/customized_models.py
+++ bigg/bigg/extension/customized_models.py
@@ -313,6 +313,8 @@ class BiggWithEdgeLen(RecurTreeGen):
                     states_h.append(cur_state[0])
                     #print(cur_state[0].shape)
                     states_c.append(cur_state[1])
+                print(prev_states_h[0].shape)
+                print(prev_states_h[0][0].shape)
                 prev_states_h = [torch.cat([h[i] for h in prev_states_h if h.shape[0] > i], dim = 0) for i in range(0, B)]
                 print(prev_states_h[0].shape)
                 print(prev_states_h[-1].shape)
