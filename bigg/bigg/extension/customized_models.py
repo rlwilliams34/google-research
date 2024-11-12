d9f8e4841c562256241a217bdebd08039ba9be8a
rlwilliams34 <rlwilliams34@g.ucla.edu>
2024-11-11 20:57:28 -0800

    test

--- bigg/bigg/extension/customized_models.py
+++ bigg/bigg/extension/customized_models.py
@@ -292,6 +292,7 @@ class BiggWithEdgeLen(RecurTreeGen):
                         state_idx = idx
                     else:
                         state_idx = idx[prev_idx]
+                        prev_idx = idx
                     edge = edge[idx]
                     edge = self.edgelen_encoding(edge.unsqueeze(-1))
                     cur_state = self.edgeLSTM(edge, (cur_state[0][state_idx], cur_state[1][state_idx]))
