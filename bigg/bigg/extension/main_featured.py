62fe83fce33e5c9219624840f4fc4a2360121bec
rlwilliams34 <rlwilliams34@g.ucla.edu>
2024-12-12 16:10:10 -0800

    fix

--- bigg/bigg/extension/main_featured.py
+++ bigg/bigg/extension/main_featured.py
@@ -200,11 +200,10 @@ if __name__ == '__main__':
     with open(path, 'rb') as f:
         train_graphs = cp.load(f)
     
-    print("Training Graph Example: ")
-    print(train_graphs[0].edges())
-    print(STOP)
     max_num_nodes = max([len(gg.nodes) for gg in train_graphs])
     cmd_args.max_num_nodes = max_num_nodes
+    
+    print(train_graphs[1].edges())
     
     if cmd_args.phase == "train": 
         [TreeLib.InsertGraph(g) for g in train_graphs]
