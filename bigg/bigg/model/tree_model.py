1989f4d1b997a1d38b5d66a345c714ac158130d4
rlwilliams34 <rlwilliams34@g.ucla.edu>
2024-12-21 15:05:53 -0500

    fix

--- bigg/bigg/model/tree_model.py
+++ bigg/bigg/model/tree_model.py
@@ -737,7 +737,7 @@ class RecurTreeGen(nn.Module):
                 new_h, new_c = featured_batch_tree_lstm2(local_edge_feats, is_rch, h_bot, c_bot, h_buf, c_buf, fn_ids, self.lr2p_cell, wt_update =self.update_wt, method = self.method)
             else:
                 new_h, new_c = batch_tree_lstm2(h_bot, c_bot, h_buf, c_buf, fn_ids, self.lr2p_cell)
-            if self.method == "Test4" and d == 0:
+            if self.method in ["Test4", "Teat5"] and d == 0:
                 b = self.batch_size
                 m = edge_feats[0].shape[0] // b 
                 idx = ([False] + [True]*(m-1))*b
@@ -745,9 +745,9 @@ class RecurTreeGen(nn.Module):
                 edge_embed_cur = (edge_feats[0][idx], edge_feats[1][idx])
                 new_h, new_c = self.merge_top_wt((new_h, new_c), edge_embed_cur)
             
-            elif self.method == "Test5" and d == 0:
-                edge_embed_cur = (edge_feats[0][idx], edge_feats[1][idx])
-                new_h, new_c = self.merge_top_wt((new_h, new_c), edge_embed_cur)
+            #elif self.method == "Test5" and d == 0:
+            #    edge_embed_cur = (edge_feats[0][idx], edge_feats[1][idx])
+            #    new_h, new_c = self.merge_top_wt((new_h, new_c), edge_embed_cur)
             
             h_buf_list[d] = new_h
             c_buf_list[d] = new_c
