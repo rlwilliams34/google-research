ebb0897ebaf4e298425eb0bfd92f74a22c954122
rlwilliams34 <rlwilliams34@g.ucla.edu>
2025-01-13 12:16:58 -0800

    fix

--- bigg/bigg/model/tree_model.py
+++ bigg/bigg/model/tree_model.py
@@ -103,7 +103,7 @@ def featured_batch_tree_lstm2(edge_feats, is_rch, h_bot, c_bot, h_buf, c_buf, fn
         leaf_check = is_leaf[i]
         print(leaf_check)
         local_hbot, local_cbot = h_bot[:, leaf_check], c_bot[:, leaf_check]
-        print(local_hbot.shape)
+        #print(local_hbot.shape)
         
         if method == "Test8":
             dev = local_hbot.device
@@ -121,6 +121,7 @@ def featured_batch_tree_lstm2(edge_feats, is_rch, h_bot, c_bot, h_buf, c_buf, fn
             local_hbot, local_cbot = cell_node(node_feats[i], (local_hbot, local_cbot))
         
         h_vecs, c_vecs = tree_state_select(local_hbot, local_cbot, h_buf, c_buf, lambda : new_ids[i])
+        print("H VEC SHAPE")
         print(h_vecs.shape)
         h_list.append(h_vecs)
         c_list.append(c_vecs)
