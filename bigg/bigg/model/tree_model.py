11ed3a96fc80a294df414655f0b73cf48cf57937
rlwilliams34 <rlwilliams34@g.ucla.edu>
2025-01-13 11:03:19 -0800

    fix

--- bigg/bigg/model/tree_model.py
+++ bigg/bigg/model/tree_model.py
@@ -386,7 +386,7 @@ class RecurTreeGen(nn.Module):
 
         if self.share_param:
             self.m_l2r_cell = BinaryTreeLSTMCell(args.embed_dim)
-            if self.method == "Test7":
+            if self.method == "Test8":
                 self.lr2p_cell = WeightedBinaryTreeLSTMCell(args.embed_dim, args.wt_embed_dim)
             
             else:
