47f4ddefd9ba5819654bad663345b5765db128d8
rlwilliams34 <rlwilliams34@g.ucla.edu>
2025-01-13 23:54:16 -0800

    fix

--- bigg/bigg/extension/customized_models.py
+++ bigg/bigg/extension/customized_models.py
@@ -267,7 +267,7 @@ class BiggWithEdgeLen(RecurTreeGen):
         if self.method == "Test9":
             K = edge_embed.shape[0]
             x_in = torch.cat([self.leaf_embed.repeat(K, 1), edge_embed], dim = -1)
-            s_in = (self.test_h0.repeat(1, K, 1), self.test_c0.repeat(1, K, 1)))
+            s_in = (self.test_h0.repeat(1, K, 1), self.test_c0.repeat(1, K, 1))
             edge_embed = self.leaf_LSTM(x_in, s_in)
         
         
