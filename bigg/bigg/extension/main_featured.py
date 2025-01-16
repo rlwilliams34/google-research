8a97bc9fbdae40674204194270346ae40b40b51a
rlwilliams34 <rlwilliams34@g.ucla.edu>
2025-01-16 09:36:40 -0800

    test

--- bigg/bigg/extension/main_featured.py
+++ bigg/bigg/extension/main_featured.py
@@ -499,15 +499,6 @@ if __name__ == '__main__':
                 #edge_feats_embed_c = (torch.cat([list_edge_feats_embed[1][i] for i in batch_indices], dim=1)) #[list_edge_feats[i] for i in batch_indices]
                 #edge_feats_embed = (edge_feats_embed_h, edge_feats_embed_c)
                 edge_feats = [list_edge_feats[i] for i in batch_indices]
-                print("LIST RC")
-                print(list_rc)
-                print(STOP)
-                if list_rc is not None:
-                    print("Hello!")
-                    rc = [list_rc[i] for i in batch_indices]
-                    print(rc)
-                    print(STOP)
-                    edge_feats = (edge_feats, rc)
                 
             elif cmd_args.method == "Test4":
                 edge_feats = [list_edge_feats[i] for i in batch_indices]
@@ -517,6 +508,11 @@ if __name__ == '__main__':
             else:
                 edge_feats = (torch.cat([list_edge_feats[i] for i in batch_indices], dim=0) if list_edge_feats is not None else None)
             
+            if list_rc is not None:
+                rc = np.concatenate([list_rc[i] for i in batch_indices], dim=0)
+                edge_feats = (edge_feats, rc)
+                
+                
             if cmd_args.sigma:
                 batch_idx = np.concatenate([np.repeat(i, len(train_graphs[i])) for i in batch_indices])
             
