8cc659b1a750f3dc49cf13df4b3992d86657f300
rlwilliams34 <rlwilliams34@g.ucla.edu>
2025-01-16 08:52:27 -0800

    fix

--- bigg/bigg/model/tree_model.py
+++ bigg/bigg/model/tree_model.py
@@ -482,9 +482,7 @@ class RecurTreeGen(nn.Module):
                     x_in = torch.cat([self.empty_embed, torch.zeros(self.empty_embed.shape).to(self.empty_embed.device)], dim = -1)
                 
                 else:
-                    print(self.empty_embed.shape)
-                    print(STOP)
-                    x_in = torch.cat([self.empty_embed, torch.zeros(self.empty_embed.shape).to(self.empty_embed.device)], dim = -1)
+                    x_in = torch.cat([self.empty_embed, torch.zeros(1, 3 * self.empty_embed.shape[-1]).to(self.empty_embed.device)], dim = -1)
                 return self.leaf_LSTM(x_in, (self.test_h0, self.test_c0))
                 self.empty_h0, self.empty_c0 = self.leaf_LSTM(x_in, (self.test_h0, self.test_c0))
             return (self.empty_h0, self.empty_c0)
