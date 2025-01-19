6fb57d2ab22ad39715d7f3f929463f41ef8369b1
rlwilliams34 <rlwilliams34@g.ucla.edu>
2025-01-18 18:00:12 -0800

    Test

--- bigg/bigg/model/tree_model.py
+++ bigg/bigg/model/tree_model.py
@@ -139,6 +139,7 @@ def featured_batch_tree_lstm3(feat_dict, h_bot, c_bot, h_buf, c_buf, h_past, c_p
     if 'node' in feat_dict:
         t_lch, t_rch = feat_dict['node']
     if h_past is None:
+        print("Hello Test")
         return featured_batch_tree_lstm2(edge_feats, is_rch, h_bot, c_bot, h_buf, c_buf, lambda i: fn_all_ids(i)[:-2], cell, t_lch, t_rch, cell_node)
     elif h_bot is None:
         return batch_tree_lstm2(h_buf, c_buf, h_past, c_past, lambda i: fn_all_ids(i)[2:], cell)
@@ -233,6 +234,9 @@ class FenwickTree(nn.Module):
         print(row_embeds[0][0].shape)
         print(row_embeds[1][0].shape)
         print(row_embeds[2][0].shape)
+        
+        ### Have all edge embeddings (except last edge ...?)
+        
         
         for i, all_ids in enumerate(tree_agg_ids):
             fn_ids = lambda x: all_ids[x]
