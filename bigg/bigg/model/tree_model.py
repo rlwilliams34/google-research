a1a24e544b6367e6d3c5ea1b057405284acaf6cd
rlwilliams34 <rlwilliams34@g.ucla.edu>
2025-01-20 10:51:51 -0800

    fix

--- bigg/bigg/model/tree_model.py
+++ bigg/bigg/model/tree_model.py
@@ -749,7 +749,7 @@ class RecurTreeGen(nn.Module):
             if self.has_edge_feats:
                 edge_idx, is_rch = TreeLib.GetEdgeAndLR(lv + 1)
                 left_feats = (edge_feats_embed[0][:, edge_idx[~is_rch]], edge_feats_embed[1][:, edge_idx[~is_rch]])
-                h_bot, c_bot = h_bot[left_ids[0]], c_bot[left_ids[0]]
+                h_bot, c_bot = h_bot[:, left_ids[0]], c_bot[:, left_ids[0]]
                 h_bot, c_bot = selective_update_hc(h_bot, c_bot, left_ids[0], left_feats)
                 left_ids = tuple([None] + list(left_ids[1:]))
 
