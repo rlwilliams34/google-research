1ae90f5b9d474225d68fa71e696ce3dfaf71ae53
rlwilliams34 <rlwilliams34@g.ucla.edu>
2025-01-20 09:59:34 -0800

    test

--- bigg/bigg/model/tree_model.py
+++ bigg/bigg/model/tree_model.py
@@ -721,7 +721,8 @@ class RecurTreeGen(nn.Module):
             edge_feats_embed = self.embed_edge_feats(edge_feats)
         logit_has_edge = self.pred_has_ch(row_states[0][-1])
         has_ch, _ = TreeLib.GetChLabel(0, dtype=bool)
-        ll_cur, ll_batch = ll + self.binary_ll(logit_has_edge, has_ch)
+        ll_cur, ll_batch = self.binary_ll(logit_has_edge, has_ch, batch_idx = batch_idx, ll_batch = ll_batch)
+        ll = ll + ll_cur
         cur_states = (row_states[0][:, has_ch], row_states[1][:, has_ch])
         
         if batch_idx is not None:
