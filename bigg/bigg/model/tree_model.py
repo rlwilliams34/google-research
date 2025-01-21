ca87167053e2df72d147460b6710617c51960045
rlwilliams34 <rlwilliams34@g.ucla.edu>
2025-01-21 11:05:14 -0800

    test

--- bigg/bigg/model/tree_model.py
+++ bigg/bigg/model/tree_model.py
@@ -787,7 +787,7 @@ class RecurTreeGen(nn.Module):
         self.row_tree.reset(list_states)
         controller_state = self.row_tree()
         if self.method == "Test285":
-            self.weight_tree()
+            self.weight_tree.reset([])
         if num_nodes is None:
             num_nodes = node_end
         pbar = range(node_start, node_end)
