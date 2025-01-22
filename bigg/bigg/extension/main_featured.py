cb5c840ff6470bcbe1dfe4d7258e626f185382ff
rlwilliams34 <rlwilliams34@g.ucla.edu>
2025-01-22 14:45:17 -0800

    fix

--- bigg/bigg/extension/main_featured.py
+++ bigg/bigg/extension/main_featured.py
@@ -434,7 +434,7 @@ if __name__ == '__main__':
                     db_info += [(info1, info2)]
             
             elif cmd_args.g_type == "tree":
-                list_num_edges = [len(train_graphs[0]).edges] * cmd_args.batch_size
+                list_num_edges = [len(train_graphs[0].edges())] * cmd_args.batch_size
                 info1 = get_list_indices([list_num_edges])
                 batch_lv_list = get_batch_lv_list_fast([list_num_edges])
                 info2 = prepare_batch(batch_lv_list)
