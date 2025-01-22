7079a0d041bb06902742b6b6e13ba40d5827cd99
rlwilliams34 <rlwilliams34@g.ucla.edu>
2025-01-22 14:38:58 -0800

    fix

--- bigg/bigg/extension/main_featured.py
+++ bigg/bigg/extension/main_featured.py
@@ -424,7 +424,7 @@ if __name__ == '__main__':
                 if cmd_args.method in ["Test10", "Test12"]:
                     list_rc = [get_edge_feats(g, cmd_args.method)[1] for g in train_graphs]
             
-            if cmd_args.g_type == "db":
+            if cmd_args.g_type == "dbkm,":
                 list_num_edges = [len(g.edges()) for g in train_graphs]
                 db_info = []
                 for num_edges in list_num_edges:
