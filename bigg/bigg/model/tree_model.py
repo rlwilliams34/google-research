df7123d9a8d7001cb0a3921bcc0728288fdada4a
rlwilliams34 <rlwilliams34@g.ucla.edu>
2025-01-24 10:29:02 -0800

    fix

--- bigg/bigg/model/tree_model.py
+++ bigg/bigg/model/tree_model.py
@@ -669,7 +669,7 @@ class RecurTreeGen(nn.Module):
         if self.bits_compress:
             return self.bit_rep_net([], 1)
         else:
-            if self.method == "Test9"::
+            if self.method == "Test9":
                 x_in = torch.cat([self.empty_embed, torch.zeros(self.empty_embed.shape).to(self.empty_embed.device)], dim = -1)
                 return self.leaf_LSTM(x_in, (self.test_h0, self.test_c0))
             return (self.empty_h0, self.empty_c0)
