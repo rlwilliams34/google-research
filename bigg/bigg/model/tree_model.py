f6c37ec6318b4e53b21f2d897a6cc412b3af67e2
rlwilliams34 <rlwilliams34@g.ucla.edu>
2025-01-25 10:24:32 -0800

    test

--- bigg/bigg/model/tree_model.py
+++ bigg/bigg/model/tree_model.py
@@ -462,12 +462,12 @@ class FenwickTree(nn.Module):
         print(cur_state[0].shape)
         ## Rows 0 --> 198; 199 --> ...
         ## Need Entries [1], [200]
-#         if list_last_edge is not None:
-#             weight_state = (edge_feats_embed_l[0][:, list_last_edge[1][0]], edge_feats_embed_l[1][:, list_last_edge[1][0]])
-#             cur_state_1 = (cur_state[0][:, list_last_edge[1][1]], cur_state[1][:, list_last_edge[1][1]])
-#             cur_state_1 = func(cur_state_1, weight_state)
-#             cur_state[0][:, list_last_edge[1][1]] = cur_state_1[0]
-#             cur_state[1][:, list_last_edge[1][1]] = cur_state_1[1]
+        if list_last_edge is not None:
+            weight_state = (edge_feats_embed_l[0][:, list_last_edge[1][0]], edge_feats_embed_l[1][:, list_last_edge[1][0]])
+            cur_state_1 = (cur_state[0][:, list_last_edge[1][1]], cur_state[1][:, list_last_edge[1][1]])
+            cur_state_1 = func(cur_state_1, weight_state)
+            cur_state[0][:, list_last_edge[1][1]] = cur_state_1[0]
+            cur_state[1][:, list_last_edge[1][1]] = cur_state_1[1]
         
         if self.has_node_feats:
             base_nodes, _ = TreeLib.GetFenwickBase()
@@ -866,7 +866,7 @@ class RecurTreeGen(nn.Module):
                 cur_state = self.row_tree.node_feat_update(target_feat_embed, cur_state)
             assert lb <= len(col_sm.indices) <= ub
             
-            if self.method == "Test75" and i > 1:
+            if self.method == "Test75" and i > 0:
                 cur_state = self.merge_top_wt(cur_state, prev_state)
             
             controller_state = self.row_tree(cur_state)
