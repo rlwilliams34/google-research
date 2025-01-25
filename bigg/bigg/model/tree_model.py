d1bde8d31059d399b9afa3050b823daf7b50dd4d
rlwilliams34 <rlwilliams34@g.ucla.edu>
2025-01-25 10:42:27 -0800

    test

--- bigg/bigg/model/tree_model.py
+++ bigg/bigg/model/tree_model.py
@@ -295,6 +295,7 @@ def featured_batch_tree_lstm2(edge_feats, is_rch, h_bot, c_bot, h_buf, c_buf, fn
         if edge_feats is not None and method != "Test75":
             if method == "Special":
                 print("Hi!")
+                print(edge_feats[i])
             local_hbot, local_cbot = selective_update_hc(local_hbot, local_cbot, leaf_check, edge_feats[i])
         if cell_node is not None:
             local_hbot, local_cbot = cell_node(node_feats[i], (local_hbot, local_cbot))
