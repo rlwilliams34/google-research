90b461b7cd11ccd8b784b82b68ff39156750f63b
rlwilliams34 <rlwilliams34@g.ucla.edu>
2025-01-25 17:07:37 -0800

    fix

--- bigg/bigg/model/tree_model.py
+++ bigg/bigg/model/tree_model.py
@@ -325,7 +325,7 @@ def featured_batch_tree_lstm2(edge_feats, is_rch, h_bot, c_bot, h_buf, c_buf, fn
         
         if method == "Special" and np.sum(leaf_check) > 0:
             weight_state = (edge_embed_l[0][:, edge_embed_idx], edge_embed_l[1][:, edge_embed_idx])
-            new_local_hbot, new_local_cbot = func((local_hbot[:, 1].repeat(1, np.sum(leaf_check), 1), local_cbot[:, 1].repeat(1, npsum(leaf_check), 1)), weight_state)
+            new_local_hbot, new_local_cbot = func((local_hbot[:, 1].repeat(1, np.sum(leaf_check), 1), local_cbot[:, 1].repeat(1, np.sum(leaf_check), 1)), weight_state)
             h_vecs[:, new_ids[i][1][leaf_check == 1]] = new_local_hbot
             c_vecs[:, new_ids[i][1][leaf_check == 1]] = new_local_cbot
         h_list.append(h_vecs)
