7fd22429c9c814f60f9dc8640095714f184f4022
rlwilliams34 <rlwilliams34@g.ucla.edu>
2025-01-25 18:24:14 -0800

    fix

--- bigg/bigg/extension/customized_models.py
+++ bigg/bigg/extension/customized_models.py
@@ -39,7 +39,7 @@ class BiggWithEdgeLen(RecurTreeGen):
         assert self.sampling_method in ['gamma', 'lognormal', 'softplus']
         assert self.method in ['Test9', 'Test10', 'Test11', 'Test12', 'MLP-Repeat', 'Test285', 'Test286', 'Test287', 'Test75']
         
-        if args.has_node_fats:
+        if args.has_node_feats:
             self.nodelen_encoding = MLP(1, [2 * args.embed_dim, args.embed_dim])
             self.nodelen_pred = MLP(args.embed_dim, [2 * args.embed_dim, 1])
             self.node_state_update = nn.LSTMCell(args.embed_dim, args.embed_dim)
