ab3dd73182639cbbbddd52e0d60abff3c3425166
rlwilliams34 <rlwilliams34@g.ucla.edu>
2025-01-25 18:04:47 -0800

    test

--- bigg/bigg/model/tree_model.py
+++ bigg/bigg/model/tree_model.py
@@ -331,8 +331,8 @@ def featured_batch_tree_lstm2(edge_feats, is_rch, h_bot, c_bot, h_buf, c_buf, fn
         h_list.append(h_vecs)
         c_list.append(c_vecs)
     
-    if method == "Special": 
-        test = cell((h_list[0], c_list[0]), (h_list[1], c_list[1]))
+#     if method == "Special": 
+#         test = cell((h_list[0], c_list[0]), (h_list[1], c_list[1]))
     return cell((h_list[0], c_list[0]), (h_list[1], c_list[1]))
 
 
@@ -499,6 +499,9 @@ class FenwickTree(nn.Module):
             cur_1_idx = list_last_edge[1][1]
             weight_state = (edge_feats_embed_l[0][:, 0:1].repeat(1, len(cur_1_idx), 1), edge_feats_embed_l[1][:, 0:1].repeat(1, len(cur_1_idx), 1))
             cur_state_1 = (cur_state[0][:, cur_1_idx], cur_state[1][:, cur_1_idx])
+            print("INPUT FOR NODE 1")
+            print(cur_state[0])
+            print(weight_state[0])
             cur_state_1 = func(cur_state_1, weight_state)
             cur_state[0][:, cur_1_idx] = cur_state_1[0]
             cur_state[1][:, cur_1_idx] = cur_state_1[1]
