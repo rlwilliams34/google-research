f46af566cbb41eaf5ac6e9ce23fdf808520341d4
rlwilliams34 <rlwilliams34@g.ucla.edu>
2025-01-26 17:31:42 -0800

    more print

--- bigg/bigg/model/tree_model.py
+++ bigg/bigg/model/tree_model.py
@@ -1127,10 +1127,14 @@ class RecurTreeGen(nn.Module):
                 leaf_topdown_states = self.update_wt(leaf_topdown_states, left_feats)
                 topdown_state[0][:, left_wt_ids] = leaf_topdown_states[0]
                 topdown_state[1][:, left_wt_ids] = leaf_topdown_states[1]
-                         
+            
+            print("Current Level: ", lv)
             if self.has_edge_feats and self.method == "Test75" and np.sum(has_left) > 0:
                 cur_topdown_edge_idx = topdown_edge_index[lv]
+                print(has_left)
+                
                 left_topdown_edge_idx = cur_topdown_edge_idx[has_left.astype(bool)]
+                print(left_topdown_edge_idx)
                 has_left_states = (topdown_state[0][:, has_left.astype(bool)], topdown_state[1][:, has_left.astype(bool)])
                 left_feat = (edge_feats_embed[0][:, left_topdown_edge_idx], edge_feats_embed[1][:, left_topdown_edge_idx])
                 has_left_states = self.update_wt(has_left_states, left_feat)
