448e8e932571c4cfa4078ba76e84a7dedba2dfe6
rlwilliams34 <rlwilliams34@g.ucla.edu>
2025-01-26 23:02:31 -0800

    test

--- bigg/bigg/extension/customized_models.py
+++ bigg/bigg/extension/customized_models.py
@@ -249,7 +249,7 @@ class BiggWithEdgeLen(RecurTreeGen):
                 tot_edges = torch.sum(edge_feats > 0).item()
                 Z = torch.cumsum((edge_feats > 0).int(), 1)
                 idx_to = torch.sum(F.pad(Z[:,:-1], (1,0,0,0), mode='constant',value=0),dim=0)
-                edge_feats_normalized = edge_feats
+                edge_feats_normalized = edge_feats.clone()
                 edge_feats_normalized[edge_feats > -1] = self.standardize_edge_feats(edge_feats_normalized[edge_feats_normalized > -1])
             else:
                 B = edge_feats.shape[0]
