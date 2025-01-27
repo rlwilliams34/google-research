9186564efdde6cf6144753ee09a4299b52182be0
rlwilliams34 <rlwilliams34@g.ucla.edu>
2025-01-27 13:54:56 -0800

    test

--- bigg/bigg/model/tree_clib/tree_lib.py
+++ bigg/bigg/model/tree_clib/tree_lib.py
@@ -349,7 +349,8 @@ class _tree_lib(object):
                 edge_idx_it = np.array(mrs, dtype=np.int32)
                 mrs = [(rch[i] if rch[i] > -1 else lch[i]) for i in range(len(rch))]
                 print("mrs after: ", mrs)
-                test = [(lch[i] if lch[i]  > -1 else -1) for i in range(len(lch))]
+                test = [(lch[i] if lch[i]  > -1 else 0) for i in range(len(lch))]
+                test = [x - 1  for x in test]
                 print("Test: ", test)
                 print("=====================================")
                 
