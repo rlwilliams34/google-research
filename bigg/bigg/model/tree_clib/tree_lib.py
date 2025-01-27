b696ec4a189ab57891731c2e6e9b1f5ee50dba5b
rlwilliams34 <rlwilliams34@g.ucla.edu>
2025-01-27 15:41:27 -0800

    test

--- bigg/bigg/model/tree_clib/tree_lib.py
+++ bigg/bigg/model/tree_clib/tree_lib.py
@@ -403,7 +403,7 @@ class _tree_lib(object):
             rch_b = (rch > -1)
             print(lch_b)
             print(rch_b)
-            num_chil = lch_b.int() + rch_b.int()
+            num_chil = lch_b.astype(int) + rch_b.astype(int)
             num_chil = l
             print(num_chil)
             idx_list = list(range(num_chil))
