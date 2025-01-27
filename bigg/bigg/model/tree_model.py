3e5855389a11324ae7c494957fdedd53517b0ec6
rlwilliams34 <rlwilliams34@g.ucla.edu>
2025-01-26 16:39:47 -0800

    test

--- bigg/bigg/model/tree_model.py
+++ bigg/bigg/model/tree_model.py
@@ -1130,32 +1130,21 @@ class RecurTreeGen(nn.Module):
             left_subtree_states = [x + right_pos for x in left_subtree_states]
             topdown_state = self.l2r_cell(cur_states, left_subtree_states, lv)
             
-#             if self.has_edge_feats and self.method == "Test75" and len(left_wt_ids) > 0:
-#                 leaf_topdown_states = (topdown_state[0][:, left_wt_ids], topdown_state[1][:, left_wt_ids])
-#                 left_leaf_feats = (edge_feats_embed[0][:, ~is_rch], edge_feats_embed[1][:, ~is_rch])
-#                 left_feats_stand = self.standardize_edge_feats(edge_feats[edge_idx[~is_rch]])
-#                 leaf_topdown_states = self.update_wt(left_feats_stand, leaf_topdown_states)
-#                 topdown_state[0][:, left_wt_ids] = leaf_topdown_states[0]
-#                 topdown_state[1][:, left_wt_ids] = leaf_topdown_states[1]
-            
-            cur_topdown_edge_idx = topdown_edge_index[lv]
-#             print("Cur topdown edge idx",  cur_topdown_edge_idx)
-#             print("Has left",  has_left)
-#             
+            if self.has_edge_feats and self.method == "Test75" and len(left_wt_ids) > 0:
+                leaf_topdown_states = (topdown_state[0][:, left_wt_ids], topdown_state[1][:, left_wt_ids])
+                leaf_topdown_states = self.update_wt(leaf_topdown_states, left_feats)
+                topdown_state[0][:, left_wt_ids] = leaf_topdown_states[0]
+                topdown_state[1][:, left_wt_ids] = leaf_topdown_states[1]
+                         
             if self.has_edge_feats and self.method == "Test75" and np.sum(has_left) > 0:
+                cur_topdown_edge_idx = topdown_edge_index[lv]
                 left_topdown_edge_idx = cur_topdown_edge_idx[has_left]
                 has_left_states = (topdown_state[0][:, has_left], topdown_state[1][:, has_left])
                 left_feat = (edge_feats_embed[0][:, left_topdown_edge_idx], edge_feats_embed[1][:, left_topdown_edge_idx])
                 has_left_states = self.update_wt(has_left_states, left_feat)
                 topdown_state[0][:, has_left] = has_left_states[0]
                 topdown_state[1][:, has_left] = has_left_states[1]
-                
-
-## Challenge: Need the edge feat that corresponds to the topdown state that is being referenced...
-## For each topdown state, need the corresponding edge feature to be udpated ....
-
-## topdown_states (HAS LEFT)
-
+            
             right_logits = self.pred_has_right(topdown_state[0][-1], lv)
             right_update = self.topdown_right_embed[has_right]
             topdown_state = self.cell_topright(right_update, topdown_state, lv)
