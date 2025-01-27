786f3f619aa7d1f21a7a3c1bfc40acee696ab664
rlwilliams34 <rlwilliams34@g.ucla.edu>
2025-01-26 22:54:43 -0800

    test

--- bigg/bigg/extension/customized_models.py
+++ bigg/bigg/extension/customized_models.py
@@ -303,11 +303,14 @@ class BiggWithEdgeLen(RecurTreeGen):
                                 edge_embed_h[:, idx_to] = prev_state[0]
                                 edge_embed_c[:, idx_to] = prev_state[1]
                                 edge_feats_ret[:, idx_to] = edge_feats[i]
+                                print(edge_feats[i])
                             else: 
                                 idx_to_cur = idx_to[mask] + i
                                 edge_embed_h[:, idx_to_cur] = prev_state[0][:, mask]
                                 edge_embed_c[:, idx_to_cur] = prev_state[1][:, mask]
                                 edge_feats_ret[:, idx_to_cur] = edge_feats[i, idx_to_cur]
+                                print(edge_feats[i, idx_to_cur])
+    
                         edge_embed = (edge_embed_h, edge_embed_c)
                         print(edge_feats_ret.shape)
                         print(edge_feats_ret)
