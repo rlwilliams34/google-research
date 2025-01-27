317e4075f69112f67bfccc0eda28ad0cbd6b4b47
rlwilliams34 <rlwilliams34@g.ucla.edu>
2025-01-27 08:45:50 -0800

    fix

--- bigg/bigg/extension/main_featured.py
+++ bigg/bigg/extension/main_featured.py
@@ -767,7 +767,7 @@ if __name__ == '__main__':
     if cmd_args.epoch_load >= epoch_lr_decrease:
         cmd_args.learning_rate = 1e-4
         if cmd_args.epoch_load >= epoch_lr_decrease + 100:
-            cmd_args.learning_Rate = 1e-5
+            cmd_args.learning_rate = 1e-5
     
     print("Current Learning Rate is: ", cmd_args.learning_rate)
     print("Dividing Weight Loss by: ", cmd_args.scale_loss)
@@ -882,11 +882,10 @@ if __name__ == '__main__':
         if epoch >= epoch_lr_decrease and cmd_args.learning_rate == 1e-3:
             cmd_args.learning_rate = cmd_args.learning_rate / 10
             print("Lowering Larning Rate to: ", cmd_args.learning_rate)
-            epoch_lr_decrease += 100
             for param_group in optimizer.param_groups:
                 param_group['lr'] = cmd_args.learning_rate
         
-        elif epoch >= epoch_lr_decrease and cmd_args.learning_rate == 1e-4:
+        elif epoch >= epoch_lr_decrease + 100 and cmd_args.learning_rate == 1e-4:
             cmd_args.learning_rate = cmd_args.learning_rate / 10
             print("Lowering Larning Rate to: ", cmd_args.learning_rate)
             epoch_lr_decrease += 100
