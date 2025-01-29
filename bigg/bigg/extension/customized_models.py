63eb95d511ed61c4d547dbbc7a78a8d209042e71
rlwilliams34 <rlwilliams34@g.ucla.edu>
2025-01-29 08:22:35 -0800

    Tst

--- bigg/bigg/extension/customized_models.py
+++ bigg/bigg/extension/customized_models.py
@@ -54,7 +54,7 @@ class BiggWithEdgeLen(RecurTreeGen):
             self.node_state_update = nn.LSTMCell(args.embed_dim, args.embed_dim)
         
         self.edgelen_mean = MLP(args.embed_dim, [2 * args.embed_dim, 1], dropout = args.wt_drop)
-        self.edgelen_lvar = MLP(args.embed_dim, [2 * args.embed_dim, 1], dropout = args.wt_drop
+        self.edgelen_lvar = MLP(args.embed_dim, [2 * args.embed_dim, 1], dropout = args.wt_drop)
         
         if self.method != "Test75":
             self.leaf_embed = Parameter(torch.Tensor(1, args.weight_embed_dim))
