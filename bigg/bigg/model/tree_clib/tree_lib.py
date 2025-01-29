c428caf2fa4997414d237f9093e46a59339613b7
rlwilliams34 <rlwilliams34@g.ucla.edu>
2025-01-28 17:49:55 -0800

    no print

--- bigg/bigg/model/tree_clib/tree_lib.py
+++ bigg/bigg/model/tree_clib/tree_lib.py
@@ -392,7 +392,6 @@ class _tree_lib(object):
                     
                     left_idx[lv] = cur_left_states
                     right_idx[lv] = cur_right_states
-                print("Left index: ", left_idx)
                 return left_idx, right_idx
             
             up_lv_nonleaf = self.QueryNonLeaf(d - 1)
