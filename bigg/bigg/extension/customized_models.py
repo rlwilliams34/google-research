3dde64fc727b28f892c746cc14dff7bc58a77eaa
rlwilliams34 <rlwilliams34@g.ucla.edu>
2025-01-29 08:22:16 -0800

    Test

--- bigg/bigg/extension/customized_models.py
+++ bigg/bigg/extension/customized_models.py
@@ -54,7 +54,12 @@ class BiggWithEdgeLen(RecurTreeGen):
             self.node_state_update = nn.LSTMCell(args.embed_dim, args.embed_dim)
         
         self.edgelen_mean = MLP(args.embed_dim, [2 * args.embed_dim, 1], dropout = args.wt_drop)
-        self.edgelen_lvar = MLP(args.embed_dim, [2 * args.embed_dim, 1], dropout = args.wt_drop)
+        self.edgelen_lvar = MLP(args.embed_dim, [2 * args.embed_dim, 1], dropout = args.wt_drop
+        
+        if self.method != "Test75":
+            self.leaf_embed = Parameter(torch.Tensor(1, args.weight_embed_dim))
+            self.edgelen_encoding = MLP(1, [2 * args.weight_embed_dim, args.weight_embed_dim], dropout = args.wt_drop)
+            self.leaf_LSTM = MultiLSTMCell(2 * args.weight_embed_dim, args.embed_dim, args.rnn_layers)
         
         if self.method == "MLP-Repeat":
             self.edgelen_encoding = MLP(1, [2 * args.embed_dim, args.embed_dim], dropout = args.wt_drop)
