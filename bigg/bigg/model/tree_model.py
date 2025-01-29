07c80f46ef10a19c27c7c6c937399dd0eac3401a
rlwilliams34 <rlwilliams34@g.ucla.edu>
2025-01-29 09:45:33 -0800

    Test

--- bigg/bigg/model/tree_model.py
+++ bigg/bigg/model/tree_model.py
@@ -447,7 +447,7 @@ class FenwickTree(nn.Module):
         for i, all_ids in enumerate(tree_agg_ids):
             fn_ids = lambda x: all_ids[x]
             lstm_func = batch_tree_lstm3
-            if self.method == "Test75" or self.method == "Test85":
+            if self.method == "Test75" or self.method == "Test85" or not self.has_edge_feats:
                 has_edge_feats = False
             
             if i == 0 and self.method not in ["Test75", "Test85"] and (has_edge_feats or self.has_node_feats):
