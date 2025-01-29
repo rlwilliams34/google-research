b90a5a9a4f0a90886e0d378e436799c3925bb3f0
rlwilliams34 <rlwilliams34@g.ucla.edu>
2025-01-28 23:59:44 -0800

    TEST

--- bigg/bigg/model/tree_model.py
+++ bigg/bigg/model/tree_model.py
@@ -742,27 +742,35 @@ class RecurTreeGen(nn.Module):
                 return ll, self.bit_rep_net(tree_node.bits_rep, tree_node.n_cols), 1, None, None
             else:
                 if self.has_edge_feats:
-                    cur_feats = edge_feats[col_sm.pos - 1].unsqueeze(0) if col_sm.supervised else None
                     self.num_edge += 1
+                    cur_feats = edge_feats[col_sm.pos - 1].unsqueeze(0) if col_sm.supervised else None
                     
+                    rc = None
+                    if self.method == "Test85":
+                        col = tree_node.col_range[0]
+                        rc = np.array([col, row]).reshape(1, 1, 2)
+                        
                     if self.method in ["Test75", "Test85"] and self.num_edge > 0:
-                        rc = None
-                        if self.method == "Test85":
-                            col = tree_node.col_range[0]
-                            rc = np.array([col, row]).reshape(1, 1, 2)
                         state_update = self.update_wt(state, prev_state)
                         edge_ll, _, cur_feats = self.predict_edge_feats(state_update, cur_feats)
-                        edge_embed = self.embed_edge_feats(cur_feats, rc=rc)
-                        prev_state = edge_embed
-                        
-                        return ll, ll_wt, edge_embed, 1, cur_feats, prev_state, None
                     
                     else:
                         edge_ll, _, cur_feats = self.predict_edge_feats(state, cur_feats)
-                        ll_wt = ll_wt + edge_ll
-                        dge_embed = self.embed_edge_feats(cur_feats)
+                        
+                    ll_wt = ll_wt + edge_ll
+                    if prev_state is None:
+                        edge_embed = self.embed_edge_feats(cur_feats, prev_state=prev_state)
+                    else:
+                        if self.method in ["Test75", "Test85"]:
+                            edge_embed = self.embed_edge_feats(cur_feats, prev_state=prev_state, rc=rc)
+                            prev_state = edge_embed
+                            return ll, ll_wt, (self.leaf_h0, self.leaf_c0), 1, cur_feats, prev_state
+                        
+                        edge_embed = self.embed_edge_feats(cur_feats, prev_state=prev_state)
                         prev_state = edge_embed
+                        
                         return ll, ll_wt, (self.leaf_h0, self.leaf_c0), 1, cur_feats, prev_state
+                            
                     return ll, ll_wt, edge_embed, 1, cur_feats, prev_state, None
                     
                 else:
