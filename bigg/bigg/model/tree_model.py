649ec089c3d6c51e99c93378116d3363737463e3
rlwilliams34 <rlwilliams34@g.ucla.edu>
2025-01-29 18:14:53 -0800

    test

--- bigg/bigg/model/tree_model.py
+++ bigg/bigg/model/tree_model.py
@@ -1091,7 +1091,7 @@ class RecurTreeGen(nn.Module):
             if self.method in ["Test75", "Test85"]:
                 cur_left_updates = topdown_edge_index[0][lv]
                 print("lv: ", lv)
-                print("topdown edge index: ", topdown_edge_index)
+                print("topdown edge index: ", topdown_edge_index[0])
                 print(cur_left_updates)
                 cur_states_wt = self.merge_states(cur_left_updates, cur_states, edge_feats_embed)
                 left_logits = self.pred_has_left(cur_states_wt[0][-1], lv)
