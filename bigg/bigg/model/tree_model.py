49a258ee1d56bd78b9c1e8bb960df9c42d6ed277
rlwilliams34 <rlwilliams34@g.ucla.edu>
2025-01-29 18:08:29 -0800

    Test

--- bigg/bigg/model/tree_model.py
+++ bigg/bigg/model/tree_model.py
@@ -1051,6 +1051,7 @@ class RecurTreeGen(nn.Module):
             
         ## HERE WE NEED TO ADD AN UPDATE USING MOST. RECENT. EDGE...
         if self.method in ["Test75", "Test85"]:
+            print("SUCCESS")
             row_states_wt = self.merge_states(batch_last_edges, row_states, edge_feats_embed)
             logit_has_edge = self.pred_has_ch(row_states_wt[0][-1])
         
