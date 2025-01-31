998abb64f77c93143c044c3c702b8bcfa27d44db
rlwilliams34 <rlwilliams34@g.ucla.edu>
2025-01-31 11:25:34 -0800

    fix

--- bigg/bigg/model/tree_model.py
+++ bigg/bigg/model/tree_model.py
@@ -1050,7 +1050,7 @@ class RecurTreeGen(nn.Module):
             
             #update_idx = torch.Tensor(update_idx).to(edge_feats_embed[0].device)
         print("Update before: ", update_idx.shape)
-        update_bool = update_idx.reshape(1, update_bool.shape[0], 1)
+        update_bool = update_bool.reshape(1, update_bool.shape[0], 1)
         edge_update_idx = edge_update_idx.reshape(1, edge_update_idx.shape[0], 1)
         print("Update after: ", update_idx.shape)
         print(edge_feats_embed[0].shape)
