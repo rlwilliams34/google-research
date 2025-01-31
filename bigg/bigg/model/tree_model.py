a9627cb9441fff7fed7dd2b1f1ec7726011f1948
rlwilliams34 <rlwilliams34@g.ucla.edu>
2025-01-31 11:54:18 -0800

    test

--- bigg/bigg/model/tree_model.py
+++ bigg/bigg/model/tree_model.py
@@ -1031,6 +1031,34 @@ class RecurTreeGen(nn.Module):
             top_has_wt_states_h = scale * top_has_wt_states[0] + (1 - scale) * row_feats[0]
             top_states_wt[0][:, update_bool] = top_has_wt_states_h
             return top_states_wt[0], None
+            
+            
+            
+            
+            
+            
+        if predict_top:
+            update_bool = (update_idx != -1)
+            cur_edge_idx = update_idx[update_bool]
+        else:
+            update_bool = update_idx[0]
+            edge_of_lv = update_idx[1]
+            cur_edge_idx = edge_of_lv[update_bool] - 1
+        
+        if self.wt_one_layer:
+            cur_top_h, cur_top_c = top_states[0][-1:].clone(), top_states[1][-1:].clone()
+        
+        else:
+            cur_top_h, cur_top_c = top_states[0].clone(), top_states[1].clone()
+        test_top_states_wt = (cur_top_h, cur_top_c)
+        test_top_has_wt_states = (test_top_states_wt[0][:, update_bool], test_top_states_wt[1][:, update_bool])
+        test_row_feats = (edge_feats_embed[0][:, cur_edge_idx], edge_feats_embed[1][:, cur_edge_idx])
+        test_top_has_wt_states_h, _ = self.update_wt(test_top_has_wt_states, test_row_feats)
+        
+        
+        
+        
+        
         
         dev = edge_feats_embed[0].device
         if predict_top:
@@ -1049,7 +1077,12 @@ class RecurTreeGen(nn.Module):
         edge_feats = [torch.gather(x, 1, edge_update_idx) for x in edge_feats_embed]
         top_has_wt_states = [torch.masked_select(x, update_bool) for x in top_states]
         top_has_wt_states = [x.reshape(self.num_layers, edge_update_idx.shape[1], self.embed_dim) for x in top_has_wt_states]
-                
+        
+        print("CHECKPOINT ONE")
+        print(torch.sum((test_top_has_wt_states[0] - top_has_wt_states[0])**2))
+        print(torch.sum((test_edge_feats[0] - edge_feats[0])**2))
+        
+        
         top_has_wt_states_h, _ = self.update_wt(top_has_wt_states, edge_feats)
         top_states_wt = torch.masked_scatter(torch.zeros_like(top_states[0]), update_bool, top_has_wt_states_h)
         top_states_wt = top_states_wt.masked_scatter(~update_bool, top_states[0])
@@ -1060,29 +1093,11 @@ class RecurTreeGen(nn.Module):
         
         
         
-#         if predict_top:
-#             update_bool = (update_idx != -1)
-#             cur_edge_idx = update_idx[update_bool]
-#         else:
-#             update_bool = update_idx[0]
-#             edge_of_lv = update_idx[1]
-#             cur_edge_idx = edge_of_lv[update_bool] - 1
-#         
-#         if self.wt_one_layer:
-#             cur_top_h, cur_top_c = top_states[0][-1:].clone(), top_states[1][-1:].clone()
-#         
-#         else:
-#             cur_top_h, cur_top_c = top_states[0].clone(), top_states[1].clone()
-#         top_states_wt = (cur_top_h, cur_top_c)
-#         top_has_wt_states = (top_states_wt[0][:, update_bool], top_states_wt[1][:, update_bool])
-#         row_feats = (edge_feats_embed[0][:, cur_edge_idx], edge_feats_embed[1][:, cur_edge_idx])
-#         top_has_wt_states_h, _ = self.update_wt(top_has_wt_states, row_feats)
-#         top_states_wt[0][:, update_bool] = top_has_wt_states_h
-#                 
-#         #zero_one = torch.tensor(update_bool, dtype=torch.bool).to(cur_top_h.device).unsqueeze(1)
-#         #cur_top_h = torch.where(zero_one, top_has_wt_states_h, cur_top_h)
-#         #c = torch.where(zero_one, local_edge_feats_c, c)
-#         #top_states_wt[1][:, update_bool] = top_has_wt_states[1]
+                
+        #zero_one = torch.tensor(update_bool, dtype=torch.bool).to(cur_top_h.device).unsqueeze(1)
+        #cur_top_h = torch.where(zero_one, top_has_wt_states_h, cur_top_h)
+        #c = torch.where(zero_one, local_edge_feats_c, c)
+        #top_states_wt[1][:, update_bool] = top_has_wt_states[1]
 #         return top_states_wt[0], _
         
     def forward_row_summaries(self, graph_ids, node_feats=None, edge_feats=None,
