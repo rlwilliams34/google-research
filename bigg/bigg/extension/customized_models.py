2011fca7b0932c2efd5e475f02d3d826de6eae15
rlwilliams34 <rlwilliams34@g.ucla.edu>
2025-02-01 10:30:24 -0800

    test

--- bigg/bigg/extension/customized_models.py
+++ bigg/bigg/extension/customized_models.py
@@ -325,10 +325,10 @@ class BiggWithEdgeLen(RecurTreeGen):
 #                         print(rc)
 #                         print(rc.dtype)
                                         
-                    edge_row = rc[:, 0]
-                    edge_col = rc[:, 1]
+                    edge_row = 0 * rc[:, 0]
+                    edge_col = 0 * rc[:, 1]
                     edge_pos = self.edge_pos_enc(edge_row, edge_col)
-                    edge_embed = torch.cat([edge_embed, 0 * edge_pos], dim = -1)
+                    edge_embed = torch.cat([edge_embed, edge_pos], dim = -1)
                     
 #                     if self.num_edge <= 10 or list_num_edges is not None:
 #                         print(self.num_edge)
