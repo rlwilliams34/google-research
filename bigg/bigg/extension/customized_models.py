d1254c2a3cc360acf6dd2e9b19f6da888ad11d75
rlwilliams34 <rlwilliams34@g.ucla.edu>
2025-02-01 10:06:48 -0800

    Test

--- bigg/bigg/extension/customized_models.py
+++ bigg/bigg/extension/customized_models.py
@@ -339,16 +339,8 @@ class BiggWithEdgeLen(RecurTreeGen):
                 edge_embed = (edge_embed[0][-1:], edge_embed[1][-1:])
             
             if list_num_edges is None:
-                if self.num_edge <= 10:
-                    print(self.num_edge)
-                    print(edge_feats)
-                    print(rc)
-                    print(rc.shape)
                 edge_embed = self.weight_tree(edge_embed)
             else:
-                print(rc)
-                print(rc.shape)
-                print(edge_feats)
                 edge_embed = self.weight_tree.forward_train_weights(edge_embed, list_num_edges, db_info)
             return edge_embed 
         
