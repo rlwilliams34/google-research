ac9a7349a21d33343fc407a3d1dc5e36c653a296
rlwilliams34 <rlwilliams34@g.ucla.edu>
2025-02-01 09:30:42 -0800

    Test

--- bigg/bigg/model/tree_model.py
+++ bigg/bigg/model/tree_model.py
@@ -748,7 +748,7 @@ class RecurTreeGen(nn.Module):
                     cur_feats = edge_feats[col_sm.pos - 1].unsqueeze(0) if col_sm.supervised else None
                     col = tree_node.col_range[0]
                     #rc = np.array([row * (row - 1) // 2 + col]).reshape(1, 1)
-                    rc = np.array([col, row]).reshape(1, 2)
+                    rc = np.array([row, col]).reshape(1, 2)
                     if prev_state is not None: #False and self.method in ["Test75", "Test85"] and self.num_edge > 0:
                         if self.add_states:
                             scale = torch.sigmoid(self.scale_wts)
