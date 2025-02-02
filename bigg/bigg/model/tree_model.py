d53c7b5ccba56840f7ef4ad4cbb90209de4f1a8f
rlwilliams34 <rlwilliams34@g.ucla.edu>
2025-02-02 09:49:27 -0800

    Test

--- bigg/bigg/model/tree_model.py
+++ bigg/bigg/model/tree_model.py
@@ -953,7 +953,11 @@ class RecurTreeGen(nn.Module):
         all_ids = TreeLib.PrepareTreeEmbed()
         if self.has_node_feats:
             node_feats = self.embed_node_feats(node_feats)
-
+            
+        if self.has_edge_feats and self.method in ["Test75", "Test85"]:
+            left_idx, right_idx = TreeLib.GetMostRecentWeight(len(all_ids) + 1, batch_last_edges=batch_last_edges)
+            topdown_edge_index = (left_idx, right_idx)
+        
         if not self.bits_compress:
             empty_h0, empty_c0 = self.get_empty_state()
             h_bot = torch.cat([empty_h0, self.leaf_h0], dim=1)
@@ -967,9 +971,6 @@ class RecurTreeGen(nn.Module):
         h_buf_list = [None] * (len(all_ids) + 1)
         c_buf_list = [None] * (len(all_ids) + 1)
         
-        if self.method in ["Test75", "Test85"]:
-            left_idx, right_idx = TreeLib.GetMostRecentWeight(len(all_ids) + 1, batch_last_edges=batch_last_edges)
-            topdown_edge_index = (left_idx, right_idx)
         
         for d in range(len(all_ids) - 1, -1, -1):
             fn_ids = lambda i: all_ids[d][i]
