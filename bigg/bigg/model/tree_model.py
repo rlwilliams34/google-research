f6778ef9345613b47eceeba70d81a5781bce6c91
rlwilliams34 <rlwilliams34@g.ucla.edu>
2025-02-02 18:48:19 -0800

    Test

--- bigg/bigg/model/tree_model.py
+++ bigg/bigg/model/tree_model.py
@@ -820,7 +820,6 @@ class RecurTreeGen(nn.Module):
                     
                     else:
                         edge_embed = self.embed_edge_feats(cur_feats, prev_state=prev_state)
-                        prev_state = edge_embed
                         return ll, ll_wt, (self.leaf_h0, self.leaf_c0), 1, cur_feats, prev_state
                             
                     return ll, ll_wt, edge_embed, 1, cur_feats, prev_state, None
